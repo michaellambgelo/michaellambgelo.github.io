d55b3e61c32db86ab395187927f06c760734a543
Michael Lamb <michael@michaellamb.dev>
2025-08-07 15:45:32 -0500

    Update grafana-faro.js

--- js/grafana-faro.js
+++ js/grafana-faro.js
@@ -4,15 +4,14 @@
   
   console.log('isLocalDev', isLocalDev);
   var webSdkScript = document.createElement("script");
-
   // fetch the latest version of the Web-SDK from the CDN
   webSdkScript.src =
     "https://unpkg.com/@grafana/faro-web-sdk@^1.4.0/dist/bundle/faro-web-sdk.iife.js";
-
   webSdkScript.onload = () => {
-    // Configure Faro differently based on environment
+    // Configure Faro to use proxy for production, direct for development
     const faroConfig = {
-      url: "https://faro-collector-prod-us-east-0.grafana.net/collect/1fe18205219b65b8844f34747e1986fa",
+      // Use proxy in production, direct endpoint in development
+      url: "https://michaellamb.dev/faro-proxy?app=blog",
       app: {
         name: "blog",
         version: "1.0.0",
@@ -27,28 +26,25 @@
       };
     }
     
+    console.log('Faro config (blog):', faroConfig);
+    
     // Initialize Faro with the appropriate configuration
     window.GrafanaFaroWebSdk.initializeFaro(faroConfig);
-
     // Load instrumentations at the onLoad event of the web-SDK and after the above configuration.
     // This is important because we need to ensure that the Web-SDK has been loaded and initialized before we add further instruments!
     var webTracingScript = document.createElement("script");
-
     // fetch the latest version of the Web Tracing package from the CDN
     webTracingScript.src =
       "https://unpkg.com/@grafana/faro-web-tracing@^1.4.0/dist/bundle/faro-web-tracing.iife.js";
-
     // Initialize, configure (if necessary) and add the the new instrumentation to the already loaded and configured Web-SDK.
     webTracingScript.onload = () => {
       window.GrafanaFaroWebSdk.faro.instrumentations.add(
         new window.GrafanaFaroWebTracing.TracingInstrumentation()
       );
     };
-
     // Append the Web Tracing script script tag to the HTML page
     document.head.appendChild(webTracingScript);
   };
-
   // Append the Web-SDK script script tag to the HTML page
   document.head.appendChild(webSdkScript);
 })();
